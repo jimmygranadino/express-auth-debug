b32be2eec276b6d5d19426b8ee95c2ed912e226e
jimmygranadino <jimmy.granadino@gmail.com>
2020-06-30 14:22:53 -0700

    installed packages to not break

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -1,35 +1,33 @@
-// import libraries/modules
-const passort = require('passport')
-const LocalStrategy = require('passport-local').LocalStrategy
-const db = require('../models')
-const passport = require('passport')
+// import necessary libraries and modules
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const db = require('../models');
 
-//serialize user
-passport.serializeUser(function(user, cb) {
-    cb(null, user.id)
+// serialize user
+passport.serializeUser(function(user, callback) {
+    callback(null, user.id);
 })
 
-//deserialized version
-passport.deserializeUser(function(id, cb) {
+// deserialized version
+passport.deserializeUser(function(id, callback) {
     db.user.findByPk(id).then(function(user) {
-        cb(null, user)
-    }).catch(callback)
+        callback(null, user);
+    }).catch(callback);
 })
 
-// config local vars/settings
+// configure local variables/settings
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
 }, function(email, password, callback) {
-    db.userfindOne({ where: { email }}).then(function(user) {
-        if(!user || !user.validPassword(password)) {
-            callback(null, false)
+    db.user.findOne({ where: { email }}).then(function(user) {
+        if (!user || !user.validPassword(password)) {
+            callback(null, false);
         } else {
-            callback(null, user)
+            callback(null, user);
         }
-    }).catch(callback)
-}))
+    }).catch(callback);
+}));
 
-// password local config
 
-module.exports = passport
+module.exports = passport;
